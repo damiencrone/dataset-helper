ff0f67f2d394941d115db416ac4432f5467732d7
Damien Crone <damien.crone@gmail.com>
2024-07-24 15:04:03 -0400

    Added country code documentation and fixed warn arg

--- R/construct_country_mapping.R
+++ R/construct_country_mapping.R
@@ -4,6 +4,7 @@
 #'
 #' @param x A character vector of country codes.
 #' @param alpha_code Character. The desired alpha code format for the output. Default is "iso3c".
+#' @param warn Logical. Whether to show warnings when country codes cannot be matched. Default is TRUE.
 #'
 #' @return A data frame with columns:
 #'   \item{Country}{Full country name}
@@ -24,14 +25,14 @@
 #' @importFrom countrycode countrycode
 #'
 #' @export
-construct_country_mapping <- function (x, alpha_code = "iso3c") {
+construct_country_mapping <- function (x, alpha_code = "iso3c", warn = TRUE) {
   uv <- unique(x[!is.na(x)])
   scheme <- infer_countrycode_scheme(uv)
   freq_table <- table(x)
   country_mapping <- data.frame(
-    Country = countrycode::countrycode(sourcevar = uv, origin = scheme, destination = "country.name", warn = FALSE),
+    Country = countrycode::countrycode(sourcevar = uv, origin = scheme, destination = "country.name", warn = warn),
     Code = 0:(length(uv)-1),
-    Alpha_Code = countrycode::countrycode(sourcevar = uv, origin = scheme, destination = alpha_code, warn = FALSE),
+    Alpha_Code = countrycode::countrycode(sourcevar = uv, origin = scheme, destination = alpha_code, warn = warn),
     Source_Var = uv,
     Freq = as.vector(freq_table[uv])
   )
